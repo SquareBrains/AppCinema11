0f201f4891417da156dc62a77b60503130ee9672
misaruseckiy <22111992>
2023-03-22 20:58:41 +0300

    "Start code"

---
+++ app/src/main/java/com/mikerusetsky/appcinema/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mikerusetsky.appcinema
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
