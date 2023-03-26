85a41c812fa6cf98fc6a7a582167baf7024f8e99
misaruseckiy <22111992>
2023-03-26 11:17:14 +0300

    "Add buttons, image and make clicks"

--- app/src/main/java/com/mikerusetsky/appcinema/MainActivity.kt
+++ app/src/main/java/com/mikerusetsky/appcinema/MainActivity.kt
@@ -2,10 +2,38 @@ package com.mikerusetsky.appcinema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.mikerusetsky.appcinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initButtons()
     }
-}
+
+
+    private fun initButtons() {
+
+     binding.menu_button.setOnClickListener {
+            Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+        }
+        binding.favourite_button. {
+            Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+        }
+        binding.watch_later_button.setOnClickListener {
+            Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+        }
+        binding.collections_button.setOnClickListener {
+            Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+        }
+        binding.settings_button.setOnClickListener {
+            Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
+        }
+    }
