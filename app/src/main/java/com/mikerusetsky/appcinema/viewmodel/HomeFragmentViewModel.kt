7bdc59db56a7a79cbf431b7bd0ed40e54dca113e
misaruseckiy <22111992>
2023-07-19 15:50:08 +0300

    "implementation Koin"

--- app/src/main/java/com/mikerusetsky/appcinema/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/mikerusetsky/appcinema/viewmodel/HomeFragmentViewModel.kt
@@ -5,24 +5,11 @@ import androidx.lifecycle.ViewModel
 import com.mikerusetsky.appcinema.App
 import com.mikerusetsky.appcinema.domain.Film
 import com.mikerusetsky.appcinema.domain.Interactor
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class HomeFragmentViewModel : ViewModel() {
-    val filmsListLiveData = MutableLiveData<List<Film>>()
+class HomeFragmentViewModel : ViewModel(), KoinComponent {
+    val filmsListLiveData:  MutableLiveData<List<Film>> = MutableLiveData()
     //Инициализируем интерактор
-    private var interactor: Interactor = App.instance.interactor
-    init {
-        interactor.getFilmsFromApi(1, object : ApiCallback {
-            override fun onSuccess(films: List<Film>) {
-                filmsListLiveData.postValue(films)
-            }
-
-            override fun onFailure() {
-            }
-        })
-    }
-
-    interface ApiCallback {
-        fun onSuccess(films: List<Film>)
-        fun onFailure()
-    }
+    private val interactor: Interactor by inject()
 }
