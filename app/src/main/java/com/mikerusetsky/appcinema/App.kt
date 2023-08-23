228c72d02dc566344ae6e5bc9fc79bd375c4bd18
misaruseckiy <22111992>
2023-08-23 16:42:57 +0300

    "Upgrade buildgradle for implementation DaggerAppComponent"

--- app/src/main/java/com/mikerusetsky/appcinema/App.kt
+++ app/src/main/java/com/mikerusetsky/appcinema/App.kt
@@ -4,6 +4,7 @@ package com.mikerusetsky.appcinema
 
 import android.app.Application
 import com.mikerusetsky.appcinema.di.AppComponent
+import com.mikerusetsky.appcinema.di.DaggerAppComponent
 import com.mikerusetsky.appcinema.di.modules.DatabaseModule
 import com.mikerusetsky.appcinema.di.modules.DomainModule
 import com.mikerusetsky.appcinema.di.modules.RemoteModule
@@ -11,16 +12,12 @@ import com.mikerusetsky.appcinema.di.modules.RemoteModule
 class App : Application() {
     lateinit var dagger: AppComponent
 
-    override fun onCreate() {
-        super.onCreate()
-        instance = this
-        //Создаем компонент
-        dagger = DaggerAppComponent.builder()
-            .remoteModule(RemoteModule())
-            .databaseModule(DatabaseModule())
-            .domainModule(DomainModule(this))
-            .build()
-    }
+        override fun onCreate() {
+            super.onCreate()
+            instance = this
+            //Создаем компонент
+            dagger = DaggerAppComponent.builder().build()
+        }
 
     companion object {
         lateinit var instance: App
