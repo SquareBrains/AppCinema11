579089137554cb5cf14b21d791333d2c581b4c52
misaruseckiy <22111992>
2023-08-29 13:01:12 +0300

    "Upgrade code 2"

--- app/src/main/java/com/mikerusetsky/appcinema/TmdbApi.kt
+++ app/src/main/java/com/mikerusetsky/appcinema/TmdbApi.kt
@@ -1,13 +1,13 @@
 package com.mikerusetsky.appcinema
 
-import android.telecom.Call
+
 import retrofit2.http.GET
 import retrofit2.http.Query
 import com.mikerusetsky.appcinema.data.Entity.TmdbResultsDto
 import retrofit2.http.Path
 
 interface TmdbApi {
-    @GET("3/movie/popular")
+    @GET("3/movie/{category}")
     fun getFilms(
         @Path("category") category: String,
         @Query("api_key") apiKey: String,
